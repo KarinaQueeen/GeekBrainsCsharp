216eff87ab558fdbea653b4dfa5143935a60a8d0
Karina <karinanifontova06@mail.ru>
2022-09-06 09:53:54 +0300

    Seminar 2

---
+++ 2_Coding_C#/Seminar/Seminar_2/Program_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число: ");
+try
+    {
+        int a = Convert.ToInt32 (Console.ReadLine ());
+        Console.WriteLine($"Отлично, {a} целое число!");
+        int b = a * a;
+        Console.WriteLine($"Квадрат {a} = {b}."); 
+    }
+
+catch
+    {
+        Console.WriteLine("Неверный ввод."); 
+    }
---
+++ 2_Coding_C#/Seminar/Seminar_2/Program_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа показывает является ли первое число кратно второму
+
+Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+int c = a % b;
+
+if (c == 0) Console.Write ($"{a} кратно {b}.");
+else Console.Write ($"{a} не кратно {b}, остаток {c}.");
