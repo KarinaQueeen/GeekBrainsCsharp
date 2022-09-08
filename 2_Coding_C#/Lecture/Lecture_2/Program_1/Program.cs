b7c55aa78fbb250858a51ab4e9dacfc233c25930
Karina <karinanifontova06@mail.ru>
2022-09-08 16:55:57 +0300

    Lecture 1, 2

--- 2_Coding_C#/Lecture/Lecture_2/Program_1/Program.cs
+++ 2_Coding_C#/Lecture/Lecture_2/Program_1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int a1 = 4;
+int b1 = 5;
+int c1 = 6;
+int a2 = 2;
+int b2 = 3;
+int c2 = 1;
+int a3 = 8;
+int b3 = 15;
+int c3 = 0;
+
+int Max (int a, int b, int c)
+    {
+        int maxi = a;
+        if (b > maxi) maxi = b;
+        if (c > maxi) maxi = c;
+        return maxi;
+    }
+
+int max = Max( Max(a1,b1,c1), Max(a2,b2,c2), Max(a3,b3,c3));
+Console.WriteLine (max);
