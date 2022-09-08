b0d3f1cfec9793be530dc06ff726daf472575295
Karina <karinanifontova06@mail.ru>
2022-09-08 13:20:51 +0300

    Homework 3

--- 2_Coding_C#/Homework/Homework_3/Program_3/Program.cs
+++ 2_Coding_C#/Homework/Homework_3/Program_3/Program.cs
@@ -4,16 +4,16 @@ Console.Write("Введите число: ");
 int a = Convert.ToInt32 (Console.ReadLine());
 
 if (a>0)
-{
- for (int i = 1; i<=a; i++)
     {
-        double result = Math.Pow(i,3);
-        Console.WriteLine($"{i} в кубе = {result}");
-    }   
-}
+        for (int i = 1; i<=a; i++)
+            {
+                double result = Math.Pow(i,3);
+                Console.WriteLine($"{i} в кубе = {result}");
+            }   
+    }
 else
-     for (int i = a; i<=1; i++)
-    {
-        double result = Math.Pow(a,3);
-        Console.WriteLine($"{i} в кубе = {result}");
-    }  
+    for (int i = a; i<=1; i++)
+        {
+            double result = Math.Pow(a,3);
+            Console.WriteLine($"{i} в кубе = {result}");
+        }  
