5ccc6ca07eba164a69348279cbf72297485371b7
Karina <karinanifontova06@mail.ru>
2022-09-08 16:23:58 +0300

    Lecture 3

---
+++ 2_Coding_C#/Lecture/Lecture_3/Program_1/Program.cs
@@ -0,0 +1,69 @@
+﻿// 1 метод (не принимает/не возвращает)
+void Method1 ()
+    {
+        Console.WriteLine("Автор ...");
+    }
+Method1 ();    // вызывается метод со скобками
+
+
+// 2 метод (принимает/не возвращает)
+
+void Method2 (string a)
+    {
+        Console.WriteLine (a);
+    }
+Method2 ("Текст");
+
+
+void Method2 (string a, int count)
+    {
+        int i = 0;
+        while (i < count)
+            {
+                Console.WriteLine(a);
+                i++;
+            }
+    }
+Method2 ("Текст", 3);   // ("Текст", 3) или (a: "Текст",count: 3) или (count: 3, a: "Текст")
+
+
+// 3 метод (не принимает/возвращает)
+
+int Method3 ()
+    {
+        return DateTime.Now.Year;
+    }
+int year = Method3 ();
+Console.WriteLine(year);
+
+
+//4 метод (принимает / возвращает)
+
+string Method4 (int count, string text)
+    {
+        int i = 0;
+        string result = String.Empty;  //пустая строка
+
+        while (i < count)
+            {
+                result = result + text;
+                i++;
+            }
+        return result;
+    }
+
+string a = Method4 (10, "текст");
+Console.WriteLine(a);
+
+
+string Method4 (int count, string text)
+    {
+        string result = String.Empty; 
+        for (int i = 0; i < count; i++)
+            {
+                result = result + text;
+            }
+        return result;
+    }
+
+
---
+++ 2_Coding_C#/Lecture/Lecture_3/Program_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа меняет символы в тексте
+
+string Metod (string text, char oldSim, char newSim)
+{
+    string result = string.Empty;
+    int size = text.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (text [i] == oldSim ) 
+        result = result + $"{newSim}";
+        else 
+        result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string a = "В лесу родилась елочка.";
+
+string b = Metod (a, ' ', '-');
+string c = Metod (b, 'е', 'Е');
+Console.WriteLine(c);
