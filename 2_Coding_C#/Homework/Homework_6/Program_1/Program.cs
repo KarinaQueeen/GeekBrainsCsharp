366078c23cd4fa3ec28f035b0bfa5a18cad757ca
Karina <karinanifontova06@mail.ru>
2022-09-17 23:01:58 +0300

    Homework 6

--- 2_Coding_C#/Homework/Homework_6/Program_1/Program.cs
+++ 2_Coding_C#/Homework/Homework_6/Program_1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа показывает сколько положительных чисел ввел пользователь
+
+Console.Write ("Введите размер массива: ");
+int num = Convert.ToInt32(Console.ReadLine ());
+int [] array = new int [num];
+for (int i = 0; i < num; i++)
+{
+    Console.Write ($"Введите {i+1} число: ");  
+    array [i] = Convert.ToInt32(Console.ReadLine ());
+}
+
+Console.Write ("Положительных чисел в массиве: ");
+for (int i = 0; i < num; i++)
+{
+    Console.Write($"{array [i]} ");
+}
+
+int sum = 0;
+for (int i = 0; i < num; i++)
+{
+    if (array [i] > 0) sum ++;
+}
+
+Console.Write ($"= {sum} ");
--- 2_Coding_C#/Homework/Homework_6/Program_2/Program.cs
+++ 2_Coding_C#/Homework/Homework_6/Program_2/Program.cs
@@ -1,1 +1,19 @@
-﻿// 
+﻿// Программа найдет точку пересечения двух прямых, заданными уравнениями y = a1 * x + a2, y = b1 * x + b2, которые задает пользователь
+
+Console.WriteLine("Введите  2 числа для первого уравнения y = a1 * x + a2: ");
+int a1 = Convert.ToInt32 (Console.ReadLine ());
+int a2 = Convert.ToInt32 (Console.ReadLine ());
+
+Console.WriteLine("Введите  2 числа для первого уравнения y = b1 * x + b2: ");
+int b1 = Convert.ToInt32 (Console.ReadLine ());
+int b2 = Convert.ToInt32 (Console.ReadLine ());
+
+int x = (b2-a2)/(a1-b1);
+int y = a1 * x + a2;
+
+char z1 = '+';
+char z2 = '+'; 
+if (a2<0) z1 = '-';
+if (b2<0) z2 = '-';
+
+Console.WriteLine($"Точка пересечения уравнений y = {a1}x {z1} {Math.Abs(a2)}, y = {b1}x {z2} {Math.Abs(b2)} имеет координаты ({x}, {y})");
