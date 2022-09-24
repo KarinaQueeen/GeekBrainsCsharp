82342541ed9a8c52a15324238a6b9bb453d23d4d
Karina <karinanifontova06@mail.ru>
2022-09-25 00:09:51 +0300

    Lecture 6

---
+++ 2_Coding_C#/Lecture/Lecture_6/Program_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Документирование метода
+
+/// <summary>
+/// Что делает метод
+/// </summary>
+/// <param name = "n">Аргумент 1</param>
+/// <param name = "m">Аргумент 2</param>
+/// <returns>Что возвращаеь</returns>
